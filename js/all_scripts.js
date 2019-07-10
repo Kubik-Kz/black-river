7f0720fc9b5029a91efc037f1f8230e39641240c
Kubik-Kz <47062547+Kubik-Kz@users.noreply.github.com>
2019-07-10 23:44:08 +0600

    Add files via upload

--- js/all_scripts.js
+++ js/all_scripts.js
@@ -1,1 +1,97 @@
-$("#button1").click(function(){$(".popup2").toggleClass("show-block"),$(".overlay1").toggleClass("show-block")}),$(".close__form").click(function(){$(".success").toggleClass("none__form"),$(".overlay").toggleClass("none__form")}),$(".overlay").click(function(){$(".success").toggleClass("none__form"),$(".overlay").toggleClass("none__form")}),$(".menu").click(function(){$(".mobile-menu").addClass("show-block"),$(".overlay-m").addClass("show-block"),$(".mobile-menu").removeClass("none-block"),$(".overlay-m").removeClass("none-block")}),$(".close-success").click(function(){$(".pop_up-window").removeClass("visible"),$(".overlay1").addClass("none-block"),$(".overlay1").removeClass("show-block")}),$(".close-mobile-menu").click(function(){$(".mobile-menu").addClass("none-block"),$(".overlay-m").addClass("none-block")}),$(function(){$("#accordion").accordion({heightStyle:"content"})});
+$("button#call_back-pop").click(function() {
+    $(".popup_callback").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+
+$("span#call_back-pop").click(function() {
+    $(".popup_callback").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    $(".cost-layout").click(function() {
+    $(".popup_callback").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    
+    $(".block--more--1").click(function() {
+    $(".popup_showroom").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+}); 
+    
+    $(".get-approval").click(function() {
+    $(".popup_get-approval").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+}); 
+    $(".show-room").click(function() {
+    $(".popup_showroom").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    
+    $(".block--more--2").click(function() {
+    $(".popup_premium").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+     
+    $(".sells-btn").click(function() {
+    $(".popup_like").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+}); 
+
+
+    $(".discounts1").click(function() {
+    $(".popup_promo-list").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    
+    $(".btn-all-discounts").click(function() {
+    $(".popup_promo-item").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    
+    
+    
+    $(".mortgage").click(function() {
+    $(".popup_mortgage").addClass("show-block"); 
+    $(".overlay-m").addClass("show-block");
+});
+    
+    
+    $(".close-pop-up").click(function() {
+    $(".popup_callback").removeClass("show-block"); 
+    $(".popup_showroom").removeClass("show-block"); 
+    $(".popup_promo-item").removeClass("show-block"); 
+    $(".popup_mortgage").removeClass("show-block"); 
+    $(".popup_premium").removeClass("show-block"); 
+    $(".overlay-m").removeClass("show-block");
+    $(".popup_like").removeClass("show-block"); 
+    $(".popup_promo-list").removeClass("show-block");
+    $(".popup_get-approval").removeClass("show-block"); 
+});
+    
+
+    
+    
+    $(".menu").click(function() {
+    $(".mobile-menu").addClass("show-block");
+    $(".overlay-m").addClass("show-block");
+    $(".mobile-menu").removeClass("none-block");
+    $(".overlay-m").removeClass("none-block")
+});
+    
+    
+   
+    
+    $(".close-mobile-menu").click(function() {
+    $(".mobile-menu").addClass("none-block");
+    $(".overlay-m").addClass("none-block")
+});
+    
+$(document).ready(function() {
+  $("a.scrollto").click(function() {
+    var elementClick = $(this).attr("href")
+    var destination = $(elementClick).offset().top;
+    jQuery("html:not(:animated),body:not(:animated)").animate({
+      scrollTop: destination
+    }, 800);
+    return false;
+  });
+});  
